5449cc367eab7a67a91bd448a455faaa692ce680
John Doe <john@gmail.com>
2021-03-10 18:33:18 +0200

    Add BitCoin Calc

---
+++ BitCoinApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitCoinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetRates_Click(object sender, EventArgs e)
+        {
+            if (CurrencyCombo.SelectedItem.ToString() == "EUR")
+            {
+                resultLabel.Visible = true;
+                resultTextBox.Visible = true;
+                BitCoinRates bitcoin = GetRates();
+                float result = Int32.Parse(amountOfCoinBox.Text) * bitcoin.bpi.EUR.rate_float;
+                resultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.EUR.code}"; 
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static void GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+
+            {
+                var response = responseReader.ReadToEnd();
+            }
+        }
+    }
+}
